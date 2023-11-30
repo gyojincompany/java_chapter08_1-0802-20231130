6436aea988a678d9e0256e58d15bc3570396ad5e
gyojincompany <luckyred@gmail.com>
2023-11-30 11:27:05 +0900

    챕터 7-3, 8-1, 8-2 예제 추가

---
+++ src/com/jbedu/interfacetest/BusSub.java
@@ -0,0 +1,7 @@
+package com.jbedu.interfacetest;
+
+public class BusSub extends Bus{
+	
+	String a;
+
+}
---
+++ src/com/jbedu/interfacetest/Truck.java
@@ -0,0 +1,25 @@
+package com.jbedu.interfacetest;
+
+public class Truck implements Car{
+
+	@Override
+	public void excelPedal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void breakPedal() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void handle() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+}
